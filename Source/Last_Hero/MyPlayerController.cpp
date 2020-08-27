245f8e8756aa7e7cd51e0c626295fdc52b9c65d3
py6646 <py6646@naver.com>
2020-08-28 06:03:20 +0900

    1.7

--- Source/Last_Hero/MyPlayerController.cpp
+++ Source/Last_Hero/MyPlayerController.cpp
@@ -2,6 +2,26 @@
 
 
 #include "MyPlayerController.h"
+#include "ABGameplayWidget.h"
+#include "MyGameplayResultWidget.h"
+#include "MyGameStateBase.h"
+
+AMyPlayerController::AMyPlayerController()
+{
+	static ConstructorHelpers::FClassFinder< UABGameplayWidget> UI_MENU_C(TEXT("/Game/Game/UI/UI_Menu.UI_Menu_C"));
+	if (UI_MENU_C.Succeeded())
+	{
+		MenuWidgetClass = UI_MENU_C.Class;
+	}
+
+
+	static ConstructorHelpers::FClassFinder< UMyGameplayResultWidget> UI_RESULT_C(TEXT("/Game/Game/UI/UI_Result.UI_Result_C"));
+	if (UI_RESULT_C.Succeeded())
+	{
+		ResultWidgetClass = UI_RESULT_C.Class;
+	}
+
+}
 
 void AMyPlayerController::PostInitializeComponents()
 {
@@ -12,4 +32,52 @@ void AMyPlayerController::PostInitializeComponents()
 void AMyPlayerController::OnPossess(APawn* aPawn)
 {
 	Super::OnPossess(aPawn);
+}
+
+void AMyPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	InputComponent->BindAction(TEXT("GamePause"), EInputEvent::IE_Pressed, this, &AMyPlayerController::OnGamePasuse);
+}
+
+void AMyPlayerController::ChangeInputMode(bool bGameMode)
+{
+	if (bGameMode)
+	{
+		SetInputMode(GameInputMode);
+		bShowMouseCursor = false;
+	}
+	else
+	{
+		SetInputMode(UIInputMode);
+		bShowMouseCursor = true;
+	}
+}
+
+
+void AMyPlayerController::OnGamePasuse()
+{
+	MenuWidget = CreateWidget< UABGameplayWidget>(this, MenuWidgetClass);
+	MenuWidget->AddToViewport(1);
+
+	SetPause(true);
+	ChangeInputMode(false);
+}
+
+
+void AMyPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	ChangeInputMode(true);
+	ResultWidget = CreateWidget< UMyGameplayResultWidget>(this, ResultWidgetClass);
+
+}
+
+void AMyPlayerController::ShowResultUI()
+{
+	auto  AmyGameState = Cast<AMyGameStateBase> ( UGameplayStatics::GetGameState(this));
+	ResultWidget->BindGameState(AmyGameState);
+
+	ResultWidget->AddToViewport();
+	ChangeInputMode(false);
 }
--- Source/Last_Hero/MyPlayerController.h
+++ Source/Last_Hero/MyPlayerController.h
@@ -14,6 +14,31 @@ class LAST_HERO_API AMyPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 public:
+	AMyPlayerController();
+
 	virtual void PostInitializeComponents() override;
 	virtual void OnPossess(APawn* aPawn) override;
+	void ChangeInputMode(bool bGameMode = true);
+	void ShowResultUI();
+protected:
+	virtual void SetupInputComponent() override;
+	virtual void BeginPlay() override;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = UI)
+	TSubclassOf<class UABGameplayWidget> MenuWidgetClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = UI)
+	TSubclassOf<class UMyGameplayResultWidget> ResultWidgetClass;
+
+	
+private:
+	void OnGamePasuse();
+
+	UPROPERTY()
+	class UABGameplayWidget* MenuWidget;
+
+	UPROPERTY()
+	class UMyGameplayResultWidget* ResultWidget;
+
+	FInputModeGameOnly GameInputMode;
+	FInputModeUIOnly UIInputMode;
 };
